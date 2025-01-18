e2486bfa106c2eb9795d4e8a9125a3e12322c919
Ali Hosseini <ali20060101@gmail.com>
2025-01-18 15:12:21 -0800

    boilerplate

--- gs-ui/src/App.js
+++ gs-ui/src/App.js
@@ -1,23 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+    const [loading, setLoading] = useState(false);
+    const [response, setResponse] = useState("");
+    const [prompt, setPrompt] = useState("");
+    const { GoogleGenerativeAI } = require("@google/generative-ai");
+
+    const genAI = new GoogleGenerativeAI("AIzaSyDYt_4TjjbqSGaSet4FGDJibrpOTNYM51Q");
+    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+    
+    const getResponse = async (prompt) => {
+        setLoading(true);
+        const result = await model.generateContent(prompt);
+        setLoading(false);
+        setResponse(result);
+        console.log(result);
+    }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <input placeholder="Enter your prompt here"
+        onChange={(e) => setPrompt(e.target.value)}
+        style={{height: "50px", width: "200px"}}
+        value={prompt}>
+        </input>
+        <div style={{height: "50px", width: "200px", backgroundColor: "grey"}}>
+            {/*loading ? "Loading..." : response*/}
+        </div>
+        <button onClick={() => getResponse(prompt)}>Generate</button>
     </div>
   );
 }
