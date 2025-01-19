3b40efcb6d7f2a75ba3ebc6ffbae96554f79fb1b
Borna Shani <borna471@gmail.com>
2025-01-19 00:30:28 -0800

    navbar linking updated

--- gs-ui/src/components/navbar/Navbar.jsx
+++ gs-ui/src/components/navbar/Navbar.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 import styles from "../../pages/styles/home.module.css";
 import logo from "../../assets/GutSense_Logo.svg";
+import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
+  const navigate = useNavigate();
+  const handleStartForm = () => {
+    navigate("/form");
+  };
+  const handleGoToResult = () => {
+    navigate("/diagnosis");
+  };
   return (
+    
     <nav className={styles.header}>
       <div className={styles["logo-container"]}>
         <img
@@ -15,10 +24,10 @@ const Navbar = () => {
         {/* <h1 className="logo-text">GutSense</h1> */}
       </div>
       <div className={styles["menu"]}>
-        <div className={styles["question_button"]}>
+        <div className={styles["question_button"]} onClick={handleStartForm}>
           <button className={styles.menuButton}>Questionnaire</button>
         </div>
-        <div className={styles["results_button"]}>
+        <div className={styles["results_button"]} onClick={handleGoToResult}>
           <button className={styles.menuButton}>Test Results</button>
         </div>
       </div>
