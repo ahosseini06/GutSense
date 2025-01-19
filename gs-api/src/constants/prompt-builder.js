196c8baa7b5e41f344629c0e1ad5ffc4d76ad465
extreme-coder <aryan.singh.lamba@gmail.com>
2025-01-18 20:42:42 -0800

    synced api to new input schema

--- gs-api/src/constants/prompt-builder.js
+++ gs-api/src/constants/prompt-builder.js
@@ -3,17 +3,17 @@ const generatePrompt = (answers) => {
     const questions = [
         "I ______ experience bloating or gas.",
         "I ______ experience constipation or diarrhea.",
-        "I experience _______ symptoms regularly.",
+        "I experience ______ symptoms regularly.",
         "I ______ experience discomfort or pain after eating.",
         "Eating spicy or fatty foods ______ worsens my symptoms.",
         "I avoid ______ due to intolerance or discomfort.",
-        "I _______ feel fatigue, especially after meals.",
-        "I experience _______ inflammatory symptoms.",
-        "I _______ experience flare-ups of gut symptoms (e.g., diarrhea, cramping).",
+        "I ______ feel fatigue, especially after meals.",
+        "I experience ______ inflammatory symptoms.",
+        "I ______ experience flare-ups of gut symptoms (e.g., diarrhea, cramping).",
         "I ______ experience changes in my gut symptoms when under stress.",
         "My symptoms ______ wake me up at night.",
         "I ______ experience worsened symptoms after consuming processed or fried foods.",
-        "I experience _______ that may indicate gut microbiome imbalances.",
+        "I experience ______ that may indicate gut microbiome imbalances.",
         "I ______ consume fermented foods like yogurt, kimchi, or sauerkraut.",
         "I ______ take antibiotics.",
         "I ______ experience unexplained fatigue or low energy.",
@@ -23,9 +23,9 @@ const generatePrompt = (answers) => {
         "I ______ feel stressed in a typical week.",
         "I ______ experience changes (more or less) in eating patterns during periods of high stress.",
         "I ______ experience trouble falling or staying asleep.",
-        "I experience _______ during times of stress.",
+        "I experience ______ during times of stress.",
         "I ______ engage in late-night eating or snacking.",
-        "I experience _______ regularly.",
+        "I experience ______ regularly.",
         "I ______ feel overly full or uncomfortable after eating.",
         "I ______ experience fatigue despite eating balanced meals.",
         "I ______ eat meals in a rush or while distracted.",
@@ -35,7 +35,7 @@ const generatePrompt = (answers) => {
 
     // Build the paragraph by replacing the placeholders with user input
     const filledStatements = questions.map((question, index) => {
-        let response = answers[index];
+        let response = answers.filter((answer) => answer.questionId === index+1)[0].answer;
 
         // If the response is an array (multi-select), join with commas
         if (Array.isArray(response)) {
