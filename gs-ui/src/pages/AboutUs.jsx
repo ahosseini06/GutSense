c9adcb0601a8dbbe3955b4dabe7d3f7246815401
Ali Hosseini <ali20060101@gmail.com>
2025-01-19 10:14:52 -0800

    font size in about

--- gs-ui/src/pages/AboutUs.jsx
+++ gs-ui/src/pages/AboutUs.jsx
@@ -15,24 +15,28 @@ export default function AboutUs() {
       <div className={styles.container}>
         <Navbar />
         <div className={styles.body}>
-            <h1>About Us</h1>
+          <h1>About Us</h1>
+
+          <div className={styles.desc}>
             <h3>Inspiration</h3>
-
-            <div>
-            Our product was inspired by the rising prevalence of gut health conditions, and the lack of access to to specialized doctors in this field.
-            This problem is especially challenging in under-served communities with a lack of medical professionals overall. 
+            Our product was inspired by the rising prevalence of gut health
+            conditions, and the lack of access to to specialized doctors in this
+            field. This problem is especially challenging in under-served
+            communities with a lack of medical professionals overall.
             <h3>Product Vision</h3>
-            <div>
-            Our goal was to allow opportunities for anyone to gain meaningful insight into their current health condition as a first step, while also tracking their conditions over time.
-            The incorporation of Gerry also allows quick access to health insights the same way a conversation with an ER professional. 
-            Our vision is to have GutSense help telehealth companies and Hospital ER departments more effectively process patients with gut health challenges, and offering an initial diagnosis access to people in underserverd communities.
+            <div className={styles.desc}>
+              Our goal was to allow opportunities for anyone to gain meaningful
+              insight into their current health condition as a first step, while
+              also tracking their conditions over time. The incorporation of
+              Gerry also allows quick access to health insights the same way a
+              conversation with an ER professional. Our vision is to have
+              GutSense help telehealth companies and Hospital ER departments
+              more effectively process patients with gut health challenges, and
+              offering an initial diagnosis access to people in underserverd
+              communities.
             </div>
-            </div>
-
-            
-        
+          </div>
         </div>
-        
       </div>
     </>
   );
