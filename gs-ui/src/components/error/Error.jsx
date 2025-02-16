4a5b97216967973d8051a1930d38096af7f33396
Ali Hosseini <ali20060101@gmail.com>
2025-02-15 20:50:40 -0800

    better looking error message (Feb 20th)

--- gs-ui/src/components/error/Error.jsx
+++ gs-ui/src/components/error/Error.jsx
@@ -1,11 +1,24 @@
-const Error = () => <div>
-    <h1>There was an error We will make this look good later </h1>
-    <button 
-        onClick={() => window.location.href = '/'} 
-        className='btn-primary'
+import styles from "../ChatSection/ChatSection.module.css";
+const Error = () => (
+  <div
+    style={{
+      diplay: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      height: "100vh",
+      width: "100%",
+      textAlign: "center",
+    }}
+  >
+    <h1>GutSense is currently down for maintenance :{"("}</h1>
+    <p>We'll be back Feb 20th, 2025</p>
+    <button
+      onClick={() => (window.location.href = "/")}
+      className={styles.sendButton}
     >
-        Go to Home
+      Go to Home
     </button>
-</div>
+  </div>
+);
 
-export default Error
+export default Error;
